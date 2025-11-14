7ad4dbf8235d08f9241ceaf46c78bfee5dd05ef4
Witzcode0 <brijesh.gondaliya07@gmail.com>
2025-11-14 10:38:14 +0530

    datatypes, constants and var variations

---
+++ c/tokens/var-prac.c
@@ -0,0 +1,29 @@
+// Variable:
+#include<stdio.h>
+
+void main() {
+    // variable variation
+    // syntax : datatype var_name = value;
+
+    // int num1 = 10, num2 = 20;
+    // int num1 = 10, num2 = 10;
+    // int num1 = 10;
+    // float num2 = 3.13;
+    // num1 = num2;
+    // printf("%d", num1);
+
+    int num1, num2;
+
+    //printf("Enter num1: ");
+    //scanf("%d", &num1);
+
+    //printf("Enter num2: ");
+    //scanf("%d", &num2);
+
+    printf("Enter num1 & num2 sep. by space: ");
+    scanf("%d %d", &num1, &num2);
+    printf("Num1: %d,\n Num2: %d", num1, num2);
+
+
+
+}
