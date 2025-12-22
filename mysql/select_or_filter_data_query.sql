b6245da2a18c81a03e12d49e3ba59aac4fca3ad8
Witzcode0 <brijesh.gondaliya07@gmail.com>
2025-12-22 11:13:30 +0530

    select statments, operators etc

--- mysql/select_or_filter_data_query.sql
+++ mysql/select_or_filter_data_query.sql
@@ -52,3 +52,176 @@ mysql> SELECT * from likes;
 | 10 |       7 |      22 |
 | 11 |       8 |      23 |
 +----+---------+---------+
+
+mysql> select fullname, is_active from users;
++---------------+-----------+
+| fullname      | is_active |
++---------------+-----------+
+| Amit Sharma   |         1 |
+| Neha Patel    |         1 |
+| Rohit Verma   |         0 |
+| Priya Singh   |         1 |
+| Karan Mehta   |         1 |
+| Anjali Desai  |         0 |
+| Vikram Joshi  |         1 |
+| Pooja Nair    |         1 |
+| Sandeep Kumar |         0 |
+| Riya Malhotra |         1 |
++---------------+-----------+
+
+mysql> select fullname, is_active from users where is_active = 1;
++---------------+-----------+
+| fullname      | is_active |
++---------------+-----------+
+| Amit Sharma   |         1 |
+| Neha Patel    |         1 |
+| Priya Singh   |         1 |
+| Karan Mehta   |         1 |
+| Vikram Joshi  |         1 |
+| Pooja Nair    |         1 |
+| Riya Malhotra |         1 |
++---------------+-----------+
+
+mysql> select fullname from users where id < 9;
++-------------+
+| fullname    |
++-------------+
+| Amit Sharma |
+| Neha Patel  |
+| Rohit Verma |
+| Priya Singh |
+| Karan Mehta |
++-------------+
+
+mysql> select distinct is_active from users;
++-----------+
+| is_active |
++-----------+
+|         1 |
+|         0 |
++-----------+
+
+mysql> select age as price from users order by age;
++-------+
+| price |
++-------+
+|    21 |
+|    22 |
+|    24 |
+|    25 |
+|    27 |
+|    28 |
+|    30 |
+|    33 |
+|    35 |
+|    40 |
++-------+
+
+mysql> select age as price from users order by age DESC;
++-------+
+| price |
++-------+
+|    40 |
+|    35 |
+|    33 |
+|    30 |
+|    28 |
+|    27 |
+|    25 |
+|    24 |
+|    22 |
+|    21 |
++-------+
+
+mysql> select * from users where is_active = 1 and fullname LIKE "p%";
++----+-------------+-----+-----------+
+| id | fullname    | age | is_active |
++----+-------------+-----+-----------+
+|  7 | Priya Singh |  27 |         1 |
+| 11 | Pooja Nair  |  28 |         1 |
++----+-------------+-----+-----------+
+
+mysql> select * from users where is_active = 1 and fullname LIKE "%a";
++----+---------------+-----+-----------+
+| id | fullname      | age | is_active |
++----+---------------+-----+-----------+
+|  4 | Amit Sharma   |  25 |         1 |
+|  8 | Karan Mehta   |  35 |         1 |
+| 13 | Riya Malhotra |  21 |         1 |
++----+---------------+-----+-----------+
+
+mysql> select * from users where is_active = 1 and fullname LIKE "%a%";
++----+---------------+-----+-----------+
+| id | fullname      | age | is_active |
++----+---------------+-----+-----------+
+|  4 | Amit Sharma   |  25 |         1 |
+|  5 | Neha Patel    |  22 |         1 |
+|  7 | Priya Singh   |  27 |         1 |
+|  8 | Karan Mehta   |  35 |         1 |
+| 10 | Vikram Joshi  |  40 |         1 |
+| 11 | Pooja Nair    |  28 |         1 |
+| 13 | Riya Malhotra |  21 |         1 |
++----+---------------+-----+-----------+
+
+mysql> select * from users limit 3;
++----+-------------+-----+-----------+
+| id | fullname    | age | is_active |
++----+-------------+-----+-----------+
+|  4 | Amit Sharma |  25 |         1 |
+|  5 | Neha Patel  |  22 |         1 |
+|  6 | Rohit Verma |  30 |         0 |
++----+-------------+-----+-----------+
+
+mysql> select max(age) from users;
++----------+
+| max(age) |
++----------+
+|       40 |
++----------+
+
+mysql> select min(age) from users;
++----------+
+| min(age) |
++----------+
+|       21 |
++----------+
+
+mysql> select avg(age) from users;
++----------+
+| avg(age) |
++----------+
+|  28.5000 |
++----------+
+
+mysql> select count(id) from users;
++-----------+
+| count(id) |
++-----------+
+|        10 |
++-----------+
+
+SELECT *
+FROM users
+WHERE age > (SELECT AVG(age) FROM users);
+
+
+mysql> select * from users t1 inner join likes t2 on t1.id = t2.user_id;
++----+-------------+-----+-----------+----+---------+---------+
+| id | fullname    | age | is_active | id | user_id | post_id |
++----+-------------+-----+-----------+----+---------+---------+
+|  4 | Amit Sharma |  25 |         1 |  7 |       4 |      21 |
+|  5 | Neha Patel  |  22 |         1 |  8 |       5 |      21 |
+|  6 | Rohit Verma |  30 |         0 |  9 |       6 |      22 |
+|  7 | Priya Singh |  27 |         1 | 10 |       7 |      22 |
+|  8 | Karan Mehta |  35 |         1 | 11 |       8 |      23 |
++----+-------------+-----+-----------+----+---------+---------+
+
+
+-- Correct Order of SQL Clauses :
+
+-- SELECT
+-- FROM
+-- WHERE
+-- GROUP BY
+-- HAVING
+-- ORDER BY
