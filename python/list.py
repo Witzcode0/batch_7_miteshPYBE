d4865493a328751ec7c8d4c6415171e568deb5ab
Witzcode0 <brijesh.gondaliya07@gmail.com>
2026-01-03 10:26:13 +0530

    range, list

---
+++ python/list.py
@@ -0,0 +1,48 @@
+"""
+List : Mutable | Ordered | Indexing | Slicing | Duplicate values are allowed
+
+syntax:
+
+list_name = []
+list_name = list()
+
+Example : 
+list_name = [ele1, ele2, ...,elen]
+"""
+
+empty_list = []
+
+# odd = [1,3,5,7,9]
+# even = [2,4,6,8,10]
+
+# print(odd + even)
+# print(odd*2)
+
+
+nums = [1,2,3,4,5]
+# nums[0] = 10
+# print(nums)
+
+# print(dir(nums))
+
+
+# add
+# nums = [1,2,3,4,5, 1]
+# new_list = [6,7,8,9,10]
+# nums.append(new_list)
+# nums.extend(new_list)
+# nums.insert(2, new_list)
+
+# delete
+# nums.clear()
+# nums.count(1)
+# print(nums.count(1))
+# print(nums.index(1, 2))
+# del nums[3]
+# del nums
+# nums.pop()
+# nums.pop(3)
+# nums.remove(5)
+# nums.sort(reverse=True)
+# nums.reverse()
+# print(nums)
---
+++ python/range.py
@@ -0,0 +1,13 @@
+# range(start, stop+1, step+1)
+
+# left to right (positive slicing)
+# print(range(10))
+# print(list(range(10))) default start : 0
+# print(list(range(1, 10+1)))
+# print(list(range(1,10+1,1)))
+# print(list(range(1,10+1,2)))
+
+# print(list(range(-3, 4)))
+# right to left
+# print(list(range(-3, 4,-1)))
+# print(list(range(3, -4,-1)))
